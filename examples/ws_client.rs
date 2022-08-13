fc3cd888ed30dda6500e9ff53a8414d884a4d564
Doowonee <doowonee.com@gmail.com>
2022-08-13 21:30:44 +0900

    벤치마킹 테스트 프로그램 추가
    송신 수신 400MB 정도에 도달하면 앱서버랑 테스트용 클라들이
    cpu 다먹음 메모리는 충분하고 레디스는 10프로 미만 메모리도
    100메가 미만으로 사용됌 네트워크 IO는 80MB 정도
    즉 앱서버 부하로 벤치마킹 한계 도달함

--- examples/ws_client.rs
+++ examples/ws_client.rs
@@ -1,10 +1,10 @@
 //! websocket client program
 //!
 //! connect massive web socket client and broadcast message for benchmarking
+//! `RUST_LOG=info cargo run --example ws_client`
 
-use futures::{pin_mut, SinkExt, StreamExt};
-use log::info;
-use rand::prelude::*;
+use futures::{SinkExt, StreamExt};
+use log::{debug, info};
 use std::{
     sync::{Arc, Mutex},
     time::Duration,
@@ -25,17 +25,50 @@ async fn main() {
         .format_timestamp_millis()
         .init();
 
-    // 스레드간 공유되는 상태로 수신 메시지 갯수 송신 메시지 갯수 클라이언트 갯수
-    let mut status: Arc<Mutex<Status>> = Arc::new(Mutex::new(Status(0, 0, 0)));
+    // 스레드간 공유되는 상태로 수신 메시지 갯수 송신 메시지 갯수
+    let status: Arc<Mutex<Status>> = Arc::new(Mutex::new(Status(0, 0, 0)));
+    let mut handles = vec![];
+    let cloned_status = status.clone();
+
+    let show_status = tokio::spawn(async move {
+        let mut interval = time::interval(Duration::from_secs(3));
+        loop {
+            interval.tick().await;
+            info!("status: {:?}", cloned_status.lock().unwrap(),);
+        }
+    });
+    handles.push(show_status);
+
+    for _ in 1..999999 {
+        // 0.1 초씩 클라이언트 증가
+        time::sleep(Duration::from_millis(500)).await;
+        let cloned_status = status.clone();
+        let h = tokio::spawn(async move {
+            run_test(cloned_status.clone()).await;
+        });
+        handles.push(h);
+    }
+
+    for h in handles {
+        h.await.unwrap();
+    }
+    // let a = futures::future::join_all(handles);
+}
+
+pub async fn run_test(status: Arc<Mutex<Status>>) {
+    // tasks 수 더하기
+    let cloned_status = status.clone();
+    cloned_status.lock().unwrap().2 += 1;
+    drop(cloned_status);
 
     // 웹소켓 클라 만들어서 인증 하고 채팅 전송 그리고 수신시 수신 메시지 갯수 더하기
     let (ws_stream, _) = connect_async(WS_ENDPOINT).await.expect("Failed to connect");
 
     let (mut write, mut read) = ws_stream.split();
 
-    info!("수신 시작");
+    debug!("수신 시작");
     let cloned_status = status.clone();
-    let mut recv_task = tokio::spawn(async move {
+    let recv_task = tokio::spawn(async move {
         while let Some(Ok(_msg)) = read.next().await {
             cloned_status.lock().unwrap().0 += 1;
         }
@@ -50,8 +83,8 @@ async fn main() {
 
     // 0.3초 마다 랜덤 숫자 넣은 채팅 전송
     let cloned_status = status.clone();
-    let mut send_task = tokio::spawn(async move {
-        let mut interval = time::interval(Duration::from_millis(30));
+    let send_task = tokio::spawn(async move {
+        let mut interval = time::interval(Duration::from_millis(300));
         loop {
             interval.tick().await;
             let msg = format!(
@@ -64,16 +97,6 @@ async fn main() {
         }
     });
 
-    let cloned_status = status.clone();
-    let mut show_status = tokio::spawn(async move {
-        let mut interval = time::interval(Duration::from_secs(3));
-        loop {
-            interval.tick().await;
-            info!("status: {:?}", cloned_status.lock().unwrap());
-        }
-    });
-
     recv_task.await.unwrap();
     send_task.await.unwrap();
-    show_status.await.unwrap();
 }
