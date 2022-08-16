640793577616bae39e96ec0a62298b136c196cb9
Doowonee <doowonee.com@gmail.com>
2022-08-16 23:32:16 +0900

    Single redis subscribe (#21)
    기존에는 웹소켓 수신 핸들러에서 레디스 subcriber가 있어서
    모든 웹소켓 갯수만큼 레디스 on_message가 트리거되어서 뭔가
    비효율적인거 같아서 단일 세션으로 만들었는데 근데 이상한게
    이렇게 하니 오히려 도중에 서버가 응답없음 상태가 된다.
    
    테스트 프로그램 실행해보면 어느시점부터 시스템 자원률 사용현황에
    는 변함이 없는데 그냥 앱 서버가 응답이 없어 뭐지 axum 문제인지
    tokio task 문제인지도 불분명한 상황임 좀더 테스트 해봐야할듯
    
    채팅 전파시 채팅방 정보에 없는 채팅방이면 채팅 전파 되지 않도록
    했음 이러면 dos 공격 얼추 방어가능한 부분
    
    publish가 문제인지 subscribe 가 문제인지 파악하기 위해 웹소켓없이
    publish만 하는 테스트 프로그램 만듬
    
    초당 약 8천개 정도를 끝으로 더이상 부하 증가안시킴 여기서부터
    송수신 웹소켓이 1개지만 처리가 밀림 레디스도 부하좀 있고
    
    테스트 실행후 웹 브라우 웹소켓 세션에서는 송신후 수초 뒤에 수신
    된는거 원인 웹 브라우저 문제였음 서버로그상에는 처리시간 50
    마이크로세컨드 던데 그게 맞았고 웹 브라우저에 dom에 채팅내역
    겁나 많이 찍혀있어서 그게 부하여서 웹 브라우저에서 수신처리를
    늦게해서 네트워크 탭에 송신과 수신 시간차가 10초이상 나는거였음

--- examples/redis_client.rs
+++ examples/redis_client.rs
@@ -1,10 +1,13 @@
-//! websocket client program
+//! redis client program
 //!
-//! connect massive web socket client and broadcast message for benchmarking
+//! redis publish only 무조건 송신함
 //! `RUST_LOG=info cargo run --example ws_client`
 
 use chrono::Utc;
-use futures::{SinkExt, StreamExt};
+use fred::{
+    prelude::{ClientLike, PubsubInterface, RedisClient},
+    types::{ReconnectPolicy, RedisConfig},
+};
 use log::{debug, info};
 use rand::{distributions::Uniform, prelude::Distribution};
 use std::{
@@ -12,16 +15,13 @@ use std::{
     time::Duration,
 };
 use tokio::time::{self, Instant};
-use tokio_tungstenite::{connect_async, tungstenite::Message};
-
-const WS_ENDPOINT: &str = "ws://localhost:3000/websocket";
 
 #[derive(Debug)]
 pub struct Status(pub i64, pub i64, pub u32);
 
-const CLIENT_COUNT: u64 = 99999;
+const CLIENT_COUNT: u64 = 2600;
 const CLIENT_CREATING_PERIOD: u64 = 30;
-const CHAT_CREATING_PERIOD: u64 = 1333;
+const CHAT_CREATING_PERIOD: u64 = 333;
 
 #[tokio::main]
 async fn main() {
@@ -36,8 +36,14 @@ async fn main() {
     let mut handles = vec![];
     let cloned_status = status.clone();
 
+    let config = RedisConfig::default();
+    let policy = ReconnectPolicy::default();
+    let redis_client = RedisClient::new(config.clone());
+    let _ = redis_client.connect(Some(policy.clone()));
+    let _ = redis_client.wait_for_connect().await.unwrap();
+
     let show_status = tokio::spawn(async move {
-        let mut interval = time::interval(Duration::from_secs(3));
+        let mut interval = time::interval(Duration::from_secs(1));
         loop {
             interval.tick().await;
             info!("status: {:?}", cloned_status.lock().unwrap(),);
@@ -47,7 +53,6 @@ async fn main() {
 
     let mut rng = rand::thread_rng();
     let random_number: Uniform<i32> = Uniform::from(1..1000);
-
     for c in 0..CLIENT_COUNT {
         let enable_send = if c < 100 && random_number.sample(&mut rng) <= 100 {
             // 클라 100개 미만일시 10프로 확률로 전송 기능 활성화
@@ -56,11 +61,12 @@ async fn main() {
             // 100개 이상이면 1프로로 확률 변경
             c >= 100 && random_number.sample(&mut rng) <= 10
         };
+        let redis_client = redis_client.clone();
         // 0.1 초씩 클라이언트 증가
         time::sleep(Duration::from_millis(CLIENT_CREATING_PERIOD)).await;
         let cloned_status = status.clone();
         let h = tokio::spawn(async move {
-            run_test(cloned_status.clone(), enable_send).await;
+            run_test(cloned_status.clone(), enable_send, redis_client).await;
         });
         handles.push(h);
     }
@@ -71,65 +77,40 @@ async fn main() {
     // let a = futures::future::join_all(handles);
 }
 
-pub async fn run_test(status: Arc<Mutex<Status>>, enable_send: bool) {
+pub async fn run_test(status: Arc<Mutex<Status>>, _enable_send: bool, redis_client: RedisClient) {
+    // info!("enable_send: {}", enable_send);
     // tasks 수 더하기
     let cloned_status = status.clone();
     cloned_status.lock().unwrap().2 += 1;
     drop(cloned_status);
 
-    // 웹소켓 클라 만들어서 인증 하고 채팅 전송 그리고 수신시 수신 메시지 갯수 더하기
-    let (ws_stream, _) = connect_async(WS_ENDPOINT).await.expect("Failed to connect");
-
-    let (mut write, mut read) = ws_stream.split();
-
     debug!("수신 시작");
-    let cloned_status = status.clone();
-    let recv_task = tokio::spawn(async move {
-        while let Some(Ok(_msg)) = read.next().await {
-            // info!("수신 {:?}", msg);
-            cloned_status.lock().unwrap().0 += 1;
-        }
-    });
-
-    // 인증 유저 아이디 랜덤으로 다르게 지정
-    let msg = format!(
-        r#"{{"o":10,"n":"user_name","i":"s{}"}}"#,
-        rand::random::<i64>()
-    );
-    write.send(Message::from(msg)).await.unwrap();
-    // 채팅방 조인
-    write
-        .send(Message::from(r#"{"o":181,"r":"room_id"}"#))
-        .await
-        .unwrap();
 
     // 0.3초 마다 랜덤 숫자 넣은 채팅 전송
     let cloned_status = status.clone();
     let stop_watch = Instant::now();
     let send_task = tokio::spawn(async move {
         let mut interval = time::interval(Duration::from_millis(CHAT_CREATING_PERIOD));
-        if !enable_send {
-            // 송신 하는애 아니면 그냥 리턴
-            return;
-        }
         loop {
             interval.tick().await;
             let now = Utc::now().naive_utc().timestamp_millis();
             let millisecond_part = now % 1000;
+            // info!("millisecond_part: {}", millisecond_part);
             // 현재 밀리세컨드가 300초 이하면 송신 한다 송실한 클라도 랜덤이지만 매 시간마다
             // 송신 하는 행위도 랜덤으로 하도록 지정
-            if millisecond_part < 333 && stop_watch.elapsed().as_millis() > 2000 {
+            if millisecond_part < 999 && stop_watch.elapsed().as_millis() > 2000 {
                 // 2초 이내면 실행 안함 최초 실행을 막고 지정된 interval 대로 메시지 송신 하기 위함
                 let msg = format!(
                     r#"{{"o":101, "r":"room_id", "c":"{} 안녕 나는 {} 이야"}}"#,
                     rand::random::<i64>(),
                     rand::random::<i64>()
                 );
-                write.send(Message::from(msg)).await.unwrap();
+                // info!("보내기전");
+                let _received_clients: i64 = redis_client.publish("zxc", &msg).await.unwrap();
+                // info!("보냄 {}", received_clients);
                 cloned_status.lock().unwrap().1 += 1;
             }
         }
     });
-    recv_task.await.unwrap();
     send_task.await.unwrap();
 }
